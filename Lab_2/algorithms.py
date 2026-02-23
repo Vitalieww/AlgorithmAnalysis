b4ca9e508fd41eeff4fd76d90b6d27ef2023aa0a
vitalieww <vitalie.vasilean@isa.utm.md>
2026-02-23 13:05:11 +0200

    Implement mergesort

--- Lab_2/algorithms.py
+++ Lab_2/algorithms.py
@@ -21,8 +21,41 @@ def quicksort(arr):
     _quicksort(0, len(arr) - 1)
 
 def mergesort(arr):
-    # to implement
-    pass
+    """In-place MergeSort implementation."""
+
+    if len(arr) > 1:
+        mid = len(arr) // 2
+
+        # Split
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort both halves
+        mergesort(left_half)
+        mergesort(right_half)
+
+        # Merge step
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Copy remaining elements
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
 
 def heapsort(arr):
     # to implement
